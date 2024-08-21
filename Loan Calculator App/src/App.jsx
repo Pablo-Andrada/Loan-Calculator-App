61ac9447ab7af5e910ef9a548a12b334553390b6
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-08-21 08:25:20 -0300

    Inicio de proyecto, primeros componentes

--- Loan Calculator App/src/App.jsx
+++ Loan Calculator App/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
  
 
   return (
     <>
-    <h1>Loan Calculator App</h1>
+          <h1>Loan Calculator App</h1>
+          <Header titulo="Cotizador de prestamos..."/>
     </>
+
   )
 }
 
