6b00ef497a35e5da63b851ba7c91c66568082d03
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2024-08-21 14:05:56 -0300

    componentes Formulario,Resultado.Ademas helpers.js

--- Loan Calculator App/src/App.jsx
+++ Loan Calculator App/src/App.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
-import './App.css'
-import Header from './components/Header'
+import React,{useState} from 'react';
+import './App.css';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import Resultado from './components/Resultado';
+
+
 
 function App() {
- 
+  const [cantidad, setCantidad] = useState(0);
+  const [plazo, setPlazo] = useState("");
+  const [total, setTotal] = useState(0);
+
 
   return (
     <>
-          <h1>Loan Calculator App</h1>
-          <Header titulo="Cotizador de prestamos..."/>
+          
+      <Header titulo="Cotizador de prestamos" />
+      <div className="container">
+        <Formulario
+          cantidad={cantidad}
+          setCantidad={setCantidad}
+          plazo={plazo}
+          setPlazo={setPlazo}
+          setTotal={setTotal}
+        />
+      </div>
+      <div className="mensaje">
+        <Resultado
+          total={total}
+          cantidad={cantidad}
+          plazo={plazo}
+        />
+      </div>
     </>
 
   )
